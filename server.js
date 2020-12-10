778bd475ad97f99a000e752276337e5c886cc9a2
Sunber2111 <namdang3979@gmail.com>
2020-12-10 11:30:35 +0700

    fix pro 2222225

--- server.js
+++ server.js
@@ -60,10 +60,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(3000, (err) => {
+app.listen(5000, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on PORT ", 3000);
   }
 });
