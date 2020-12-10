f871a31fc72169044143849ec0dd379ae8441885
Sunber2111 <namdang3979@gmail.com>
2020-12-10 11:21:39 +0700

    test 1

--- server.js
+++ server.js
@@ -60,10 +60,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(5000, (err) => {
+app.listen(3000, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log("Listening on PORT ", 5000);
+    console.log("Listening on PORT ", 3000);
   }
 });
